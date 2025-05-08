dc95c2b06fc6445ab99ae300b3bdbac4abbd7767
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-05-09 01:13:37 +0600

    update file

--- src/components/Layouts/MainLayouts.jsx
+++ src/components/Layouts/MainLayouts.jsx
@@ -1,11 +1,20 @@
+// import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Navbar from "../Header/Navbar";
 
 const MainLayouts = () => {
 
     return (
         <div>
             {/* Navbar */}
+            <Navbar></Navbar>
             {/* Dynamic section */}
+            <div className="min-h-[]">
+
+            </div>
+            {/* <Outlet></Outlet> */}
             {/* Footer */}
+            <Footer></Footer>
 
 
         </div>
