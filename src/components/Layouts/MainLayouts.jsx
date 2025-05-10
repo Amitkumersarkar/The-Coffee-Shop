df7eeacd1270d485e89022113996c27c356b748e
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-05-10 23:39:23 +0600

    update file

--- src/components/Layouts/MainLayouts.jsx
+++ src/components/Layouts/MainLayouts.jsx
@@ -9,7 +9,7 @@ const MainLayouts = () => {
         <div>
             {/* Navbar Section*/}
             <Navbar></Navbar>
-            <div className="min-h-[calc(100vh-232px)]">
+            <div className="min-h-[calc(100vh-232px)] py-12">
                 {/* Dynamic content here */}
                 <Outlet></Outlet>
             </div>
